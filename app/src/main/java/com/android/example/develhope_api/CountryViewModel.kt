2f35fd612d69dd5b7d66e044b1af43439f75f7f3
Giovanni <121501379+Damidjo@users.noreply.github.com>
2023-06-12 16:23:12 +0200

    fix

---
+++ app/src/main/java/com/android/example/develhope_api/CountryViewModel.kt
@@ -0,0 +1,10 @@
+package com.android.example.develhope_api
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+
+class CountryViewModel : ViewModel() {
+}
--- app/src/main/java/com/android/example/develhope_api/RestCountriesApi.kt
+++ app/src/main/java/com/android/example/develhope_api/RestCountriesApi.kt
@@ -1,9 +1,8 @@
 package com.android.example.develhope_api
 
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface RestCountriesApi {
     @GET("all")
-    suspend fun getAllCountries(): ArrayList<Country>
+    suspend fun getAllCountries(): List<Country>
 }
